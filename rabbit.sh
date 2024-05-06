526c76d7fd0465bbed2965a5cc475e0079b68838
Rareshp <plesearares@protonmail.com>
2024-05-06 16:26:32 +0300

    rabbit with stomp

--- rabbit.sh
+++ rabbit.sh
@@ -5,6 +5,10 @@ case "$1" in
         echo "Starting RabbitMQ container..."
         docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
         ;;
+    start_stomp)
+        echo "Starting RabbitMQ container with STOMP plugin..."
+        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp 
+        ;;
     stop)
         echo "Stopping RabbitMQ container..."
         docker stop rabbitmq
